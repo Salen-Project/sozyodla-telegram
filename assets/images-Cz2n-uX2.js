f219cf2d9f240915695f3b508516679ef7b71509
Salen <salen@clawdbot.com>
2026-01-28 14:59:23 +0000

    Updates

---
+++ assets/images-Cz2n-uX2.js
@@ -0,0 +1,1 @@
+const n="https://xlsmwqykqkzbjqkgvljf.supabase.co",o="word-images";function r(t){if(!t)return null;const e=t.split("/").pop();return e?`${n}/storage/v1/object/public/${o}/${e}`:null}export{r as g};
